0cefaf016553c53d29e9d91bf954c05251d8bd74
fitsum-asrat <106986005+fitsum-asrat@users.noreply.github.com>
2022-07-04 23:29:07 +0300

    print 5 number

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,13 +10,13 @@
 
 int main(void)
 {
-        int num;
+	int num;
 
-        for (num = 0; num <= 9; num++)
-        {
-          printf("%d", num);
-        }
-        putchar('\n');
+	for (num = 0; num <= 9; num++)
+	{
+		printf("%d", num);
+	}
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
