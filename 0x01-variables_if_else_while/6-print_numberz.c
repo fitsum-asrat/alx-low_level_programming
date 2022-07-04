700cc98fb05162c6004ac3e50b95ebaa1b43eed2
fitsum-asrat <106986005+fitsum-asrat@users.noreply.github.com>
2022-07-04 23:30:33 +0300

    print 6 number

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,13 +10,13 @@
 
 int main(void)
 {
-        int num;
+	int num;
 
-        for (num = 0; num <= 9; num++)
-        {
-          putchar(num + '0');
-        }
-        putchar('\n');
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num + '0');
+	}
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
