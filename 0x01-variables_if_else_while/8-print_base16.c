dea7094a6542d17bac7cfdeeab90a890653f7c11
fitsum-asrat <106986005+fitsum-asrat@users.noreply.github.com>
2022-07-04 23:33:09 +0300

    print hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,19 +10,19 @@
 
 int main(void)
 {
-        char low;
-        char row;
+	char low;
+	char row;
 
-        row = 'a';
-        for (low = 0; low < 10; low++)
-        {
-          putchar(low + '0');
-        }
-        for (low = 10; low <= 15; low++)
-        {
-          putchar(row);
-          row++;
-        }
-        putchar('\n');
-        return (0);
+	row = 'a';
+	for (low = 0; low < 10; low++)
+	{
+		putchar(low + '0');
+	}
+	for (low = 10; low <= 15; low++)
+	{
+		putchar(row);
+		row++;
+	}
+	putchar('\n');
+	return (0);
 }
