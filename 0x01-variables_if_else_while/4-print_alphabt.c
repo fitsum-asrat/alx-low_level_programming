a4d880bc2249a9356b0310b9b3dd1509e997bb2c
fitsum-asrat <106986005+fitsum-asrat@users.noreply.github.com>
2022-07-04 23:27:40 +0300

    print 4 alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,17 +10,17 @@
 
 int main(void)
 {
-        char low;
+	char low;
 
-        low = 'a';
-        while (low <= 'z')
-        {
-                if (low != 'e' && low != 'q')
-                {
-                  putchar(low);
-                }
-                low++;
-        }
-        putchar('\n');
-        return (0);
+	low = 'a';
+	while (low <= 'z')
+	{
+		if (low != 'e' && low != 'q')
+		{
+			putchar(low);
+		}
+		low++;
+	}
+	putchar('\n');
+	return (0);
 }
