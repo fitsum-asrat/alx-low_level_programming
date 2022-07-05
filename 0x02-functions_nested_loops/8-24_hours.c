5c58b353f78a939f345569713b600b01c6669de9
fitsum-asrat <106986005+fitsum-asrat@users.noreply.github.com>
2022-07-06 00:26:02 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,18 +10,18 @@
 
 void jack_bauer(void)
 {
-        int H, M;
+	int H, M;
 
-        for (H = 0; H <= 23; H++)
-        {
-                for (M = 0; M <= 59; M++)
-                {
-                        _putchar((H / 10) + '0');
-                        _putchar((H % 10) + '0');
-                        _putchar(':');
-                        _putchar((M / 10) + '0');
-                        _putchar((M % 10) + '0');
-                        _putchar('\n');
-                }
-        }
+	for (H = 0; H <= 23; H++)
+	{
+		for (M = 0; M <= 59; M++)
+		{
+			_putchar((H / 10) + '0');
+			_putchar((H % 10) + '0');
+			_putchar(':');
+			_putchar((M / 10) + '0');
+			_putchar((M % 10) + '0');
+			_putchar('\n');
+		}
+	}
 }
