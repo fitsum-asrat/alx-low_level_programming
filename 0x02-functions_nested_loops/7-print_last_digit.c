d4a278a0537b058b655adf3aeae4009e75a039ed
fitsum-asrat <106986005+fitsum-asrat@users.noreply.github.com>
2022-07-06 00:23:46 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,17 +10,17 @@
 
 int print_last_digit(int n)
 {
-        int last;
+	int last;
 
-        if (n > 0)
-        {
-                last = n % 10;
-                _putchar(last + '0');
-        }
-        else
-        {
-                last = -1 * n % 10;
-                _putchar(last + '0');
-        }
-        return (last % 10);
+	if (n > 0)
+	{
+		last = n % 10;
+		_putchar(last + '0');
+	}
+	else
+	{
+		last = -1 * n % 10;
+		_putchar(last + '0');
+	}
+	return (last % 10);
 }
