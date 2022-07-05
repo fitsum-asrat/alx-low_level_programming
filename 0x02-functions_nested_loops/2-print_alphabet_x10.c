6621430c91a1048a495e6aec2a74bd7bd2b459ba
fitsum-asrat <106986005+fitsum-asrat@users.noreply.github.com>
2022-07-06 00:15:01 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,15 +8,15 @@
 
 void print_alphabet_x10(void)
 {
-        char alpha;
-        int i;
+	char alpha;
+	int i;
 
-        for (i = 0; i <= 9; i++)
-        {
-                for (alpha = 'a'; alpha <= 'z'; alpha++)
-                {
-                        _putchar(alpha);
-                }
-                _putchar('\n');
-        }
+	for (i = 0; i <= 9; i++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
 }
