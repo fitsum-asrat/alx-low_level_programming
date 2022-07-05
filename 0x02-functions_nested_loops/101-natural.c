8d84463934f7ffc02cce2e698f4980dfd974f4c5
fitsum-asrat <106986005+fitsum-asrat@users.noreply.github.com>
2022-07-06 00:35:15 +0300

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-        int i, sum = 0;
+	int i, sum = 0;
 
-        for (i = 0; i < 1024; i++)
-        {
-                if ((i % 5) == 0 || (i % 3) == 0)
-                        sum += i;
-        }
-        printf("%d\n", sum);
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 5) == 0 || (i % 3) == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
 
-        return (0);
+	return (0);
 }
